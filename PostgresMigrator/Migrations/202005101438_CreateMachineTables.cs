ecb0e896740cab4e318035b80e78c4743788c930
sbishop411 <sbishop411@gmail.com>
2020-05-11 13:27:41 -0600

    Initial implementation of FluentMigrator, along with partial implementation of initial schema.

---
+++ PostgresMigrator/Migrations/202005101438_CreateMachineTables.cs
@@ -0,0 +1,32 @@
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(202005101438)]
+    public class CreateMachineTables : Migration
+    {
+        public override void  Up()
+        {
+            // Item Tables
+            Create.Table("machine")
+                .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
+                .WithColumn("name").AsString(200).NotNullable()
+                .WithColumn("crafting_speed").AsDouble().NotNullable()
+                .WithColumn("module_slots").AsInt32().NotNullable()
+                .WithColumn("add_date").AsDateTimeOffset().NotNullable().WithDefaultValue(SystemMethods.CurrentDateTime);
+
+            Create.Table("machine_translation")
+                .WithColumn("machine_id").AsGuid().NotNullable().PrimaryKey().ForeignKey("machine", "id")
+                .WithColumn("language_id").AsGuid().NotNullable().PrimaryKey().ForeignKey("language", "id")
+                .WithColumn("display_name").AsString().NotNullable()
+                .WithColumn("add_date").AsDateTimeOffset().NotNullable().WithDefaultValue(SystemMethods.CurrentDateTime);
+        }
+
+        public override void Down()
+        {
+            // Item Tables
+            Delete.Table("machine_translation");
+            Delete.Table("machine");
+        }
+    }
+}
---
+++ PostgresMigrator/Migrations/202005111146_CreateModulesTables.cs
@@ -0,0 +1,32 @@
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(202005111146)]
+    public class CreateModulesTables : Migration
+    {
+        public override void  Up()
+        {
+            // Item Tables
+            Create.Table("module")
+                .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
+                .WithColumn("name").AsString(200).NotNullable()
+                .WithColumn("productivity_bonus").AsDouble().NotNullable()
+                .WithColumn("speed_bonus").AsDouble().NotNullable()
+                .WithColumn("add_date").AsDateTimeOffset().NotNullable().WithDefaultValue(SystemMethods.CurrentDateTime);
+
+            Create.Table("module_translation")
+                .WithColumn("module_id").AsGuid().NotNullable().PrimaryKey().ForeignKey("module", "id")
+                .WithColumn("language_id").AsGuid().NotNullable().PrimaryKey().ForeignKey("language", "id")
+                .WithColumn("display_name").AsString().NotNullable()
+                .WithColumn("add_date").AsDateTimeOffset().NotNullable().WithDefaultValue(SystemMethods.CurrentDateTime);
+        }
+
+        public override void Down()
+        {
+            // Item Tables
+            Delete.Table("module_translation");
+            Delete.Table("module");
+        }
+    }
+}
